2ae7dc0ca7ea85ded77152ba4321bd165afb613c
yagoubi oumayma <yagoubi.oumayma20@gmail.com>
2025-12-21 18:19:33 +0100

    update-file

--- task-manager-frontend/src/api/axios.js
+++ task-manager-frontend/src/api/axios.js
@@ -4,7 +4,7 @@ const api = axios.create({
   baseURL: "http://localhost:8080"
 });
 
-// Interceptor pour ajouter automatiquement le token à chaque requête
+
 api.interceptors.request.use(config => {
   const token = localStorage.getItem("token");
   if (token) {
